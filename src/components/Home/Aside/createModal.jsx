0dbd9e664c240ea569885e62ad73ea2f16d74a46
Sebastian Sala <sebastian_orko@hotmail.com>
2023-05-13 00:30:49 -0600

    posts avance

--- src/components/Home/Aside/createModal.jsx
+++ src/components/Home/Aside/createModal.jsx
@@ -1,12 +1,44 @@
 import React, { useState } from "react";
+import { auth } from "../../../firebase";
+import { collection, addDoc } from "firebase/firestore";
+import { storage } from "../../../firebase";
+import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+import { db } from "../../../firebase";
 
 export default function CreateModal({ setSelectState, selectState }) {
   const [image, setImage] = useState(undefined);
   const [text, setText] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setImage((prevState) => ({ ...prevState, Create: false }));
+    console.log(image);
+    console.log(image.name);
+    try {
+      const imageRef = ref(storage, image.name);
+      await uploadBytes(imageRef, image);
+
+      const imgUrl = await getDownloadURL(imageRef);
+      console.log(imgUrl);
+
+      const user = auth.currentUser;
+      if (user) {
+        const docRef = await addDoc(collection(db, "posts"), {
+          userID: user.uid,
+          username: user.displayName,
+          img: imgUrl,
+          txt: text,
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } else {
+        console.error("Error adding document: user not found");
+      }
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    } finally {
+      setImage(undefined);
+      setText("");
+      setSelectState((prevState) => ({ ...prevState, create: false }));
+    }
   };
 
   return (
@@ -60,13 +92,13 @@ export default function CreateModal({ setSelectState, selectState }) {
             <div className="flex flex-col items-center py-4 gap-5 flex-1">
               {image && (
                 <>
-                <div className="border w-3/4">
-                <img
-                    src={URL.createObjectURL(image)}
-                    alt="Selected image"
-                    className="w-full"
-                  />
-                </div>
+                  <div className="border w-3/4">
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt="Selected image"
+                      className="w-full"
+                    />
+                  </div>
                   <form onSubmit={(e) => handleSubmit(e)}>
                     <input
                       value={text}
--- src/components/Home/postSection.jsx
+++ src/components/Home/postSection.jsx
@@ -1,46 +1,157 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FiMoreHorizontal } from "react-icons/fi";
-import { AiOutlineHeart, AiOutlineShareAlt } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
 import { FaRegComment } from "react-icons/fa";
 import { auth } from "../../firebase";
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
+import { addDoc, collection, doc, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
 
 export default function PostSection() {
   const navigate = useNavigate();
+  const [posts, setPosts] = useState([{}]);
+  const [comments, setComments] = useState([]);
+  const [showComments, setShowComments] = useState(false);
+  const [changeComment, setChangeComment] = useState("");
+
   const signOutUser = () => {
     signOut(auth);
     navigate("/login");
   };
 
+  const handleSubmit = async (postID) => {
+    const currentUsername = auth.currentUser.displayName;
+    if(changeComment.length < 8) return;
+    try {
+      const postRef = doc(db, "posts", postID);
+      const commentRef = collection(postRef, "comments");
+      const newCommentRef = await addDoc(commentRef, {
+        username: currentUsername,
+        comment: changeComment,
+        likes: 0,
+      });
+      setChangeComment("");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleCommentChange = (postId) => {
+    const postIndex = posts.findIndex((post) => posts.id = postId);
+    const post = posts[postIndex];
+    const updatedPost = { ...post, comment: value };
+    const updatedPosts = [...posts];
+    updatedPosts.splice(postIndex, 1, updatedPost);
+    setPosts(updatedPosts);
+  }
+
+  const handleShowComments = (postId) => {
+    setShowComments(true);
+    const mapComments = async () => {
+      try {
+        const postRef = doc(db, "posts", postId);
+        const commentsRef = collection(postRef, "comments");
+        const querySnapshot = await getDocs(commentsRef);
+        const comments = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setComments({ ...comments, [postId]: comments });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    mapComments().then((data) => setComments(data));
+  };
+
+  const mapPosts = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "posts"));
+      return querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    mapPosts().then((data) => setPosts(data));
+  }, []);
+
   return (
     <section>
-      <div className="flex flex-col ml-20 mt-10 w-4/6 border-b">
-        <div>
-          <div className="flex items-center gap-3 mb-5">
-            <div className="w-8 h-8 rounded-full bg-slate-400"></div>
-            <p className="hover:text-gray-500 hover:cursor-pointer">
-              collins 3dias
-            </p>
-            <FiMoreHorizontal className="ml-auto text-xl hover:cursor-pointer hover:text-gray-500" />
+      {posts.map((post) => {
+        return (
+          <div
+            className="flex flex-col ml-20 mt-10 w-4/6 border-b pb-5"
+            key={post.id}
+          >
+            <div>
+              <div className="flex items-center gap-3 mb-5">
+                <div className="w-8 h-8 rounded-full bg-slate-400"></div>
+                <p className="hover:text-gray-500 hover:cursor-pointer">
+                  {post.username} 3dias
+                </p>
+                <FiMoreHorizontal className="ml-auto text-xl hover:cursor-pointer hover:text-gray-500" />
+              </div>
+              <img
+                src={post.img}
+                className="w-full object-cover object-center"
+              ></img>
+              <div className="flex gap-5 mt-3 items-center">
+                <AiOutlineHeart className="text-2xl hover:cursor-pointer hover:text-gray-500 max-h" />
+                <FaRegComment className="text-xl hover:cursor-pointer hover:text-gray-500 " />
+              </div>
+              <p className="hover:cursor-pointer mt-2 text-sm max">
+                {post.likes} likes
+              </p>
+              <div className="flex align-center gap-3 text-sm mt-2">
+                <span className="font-semibold">{post.username}</span>
+                <p>{post.txt}</p>
+              </div>
+              <button
+                className="block text-sm text-gray-500 "
+                onClick={() => handleShowComments(post.id)}
+              >
+                View all 2 Comments
+              </button>
+              {showComments && (
+                <div className="w-screen h-screen inset-0 bg-black/60 flex items-center justify-center z-50 absolute transition duration-300">
+                  <div
+                    className="w-full h-full"
+                    onClick={setShowComments(false)}
+                  ></div>
+                  <span
+                    className="right-4 top-1 text-white absolute text-3xl cursor-pointer"
+                    onClick={setShowComments(false)}
+                  >
+                    x
+                  </span>
+                  <div className="container  w-[40rem] h-[40rem] bg-white rounded-lg flex flex-col absolute">
+
+                  </div>
+                </div>
+              )}
+              <div className="flex flex-wrap items-center">
+                <textarea
+                  placeholder="Add a comment..."
+                  value={changeComment}
+                  onChange={(e) => handleCommentChange(e.target.value)}
+                  className="outline-none w-11/12 overflow-auto h-6 break-words max-h-24 resize-none"
+                />
+
+                <button
+                  className="text-blue-400 text-sm"
+                  onClick={() => handleSubmit(post.id)}
+                >
+                  Post
+                </button>
+              </div>
+            </div>
           </div>
-          <img
-            src="https://fotografiamejorparavendermas.com/wp-content/uploads/2017/06/La-importancia-de-la-imagen.jpg"
-            className="w-full object-cover object-center"
-          ></img>
-          <div className="flex gap-5 mt-3 items-center">
-            <AiOutlineHeart className="text-3xl hover:cursor-pointer hover:text-gray-500" />
-            <FaRegComment className="text-2xl hover:cursor-pointer hover:text-gray-500" />
-            <AiOutlineShareAlt className="text-3xl hover:cursor-pointer hover:text-gray-500" />
-          </div>
-          <p className="hover:cursor-pointer mt-2 text-sm">123 likes</p>
-          <div className="flex align-center gap-3 text-sm mt-2">
-            <span className="font-semibold">username</span>
-            <p>Tu plato Fuerte</p>
-          </div>
-          <button className="text-sm text-gray-500">View all 2 Comments</button>
-        </div>
-      </div>
+        );
+      })}
       <button
         className="text-xl mt-20 py-2 px-8 bg-red-600"
         onClick={() => signOutUser()}
